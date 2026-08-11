6c2849b6af736b83d836b8d351d190b81bbc6559
bypeakdesign <318247825+bypeakdesign@users.noreply.github.com>
2026-08-10 21:57:22 -0400

    Initial Commit

---
+++ mochiTests/mochiTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import mochi
+
+struct mochiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
